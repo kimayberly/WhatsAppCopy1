843c92ad99662fd1d5b5f3d0105fb63988b50626
kimayberly <kimberly.may.lowe@gmail.com>
2019-06-18 14:14:14 -0700

    WhatsAppCopy1

---
+++ app/src/main/java/com/lovettlowe/whatsappclone/FindUserActivity.java
@@ -0,0 +1,55 @@
+package com.lovettlowe.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public abstract class FindUserActivity extends AppCompatActivity {
+
+    private RecyclerView mUserList;
+    private RecyclerView.Adapter mUserListAdapter;
+    private RecyclerView.LayoutManager mUserListLayoutManager;
+
+    ArrayList<UserObject> userList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_user);
+
+        userList = new ArrayList<>();
+
+
+        initializerRecyclerView(); }
+
+    private void getContactList(){
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        while(phones.moveToNext()) {
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            UserObject mContact = new UserObject(name,phone);
+            userList.add(mContact);
+            mUserListAdapter.notifyDataSetChanged();
+
+        }
+
+    }
+    private void initializerRecyclerView() {
+        mUserList = findViewById(R.id.userList);
+        mUserList.setNestedScrollingEnabled(false);
+        mUserList.setHasFixedSize(false);
+        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), RecyclerView.VERTICAL, false);
+        mUserList.setLayoutManager(mUserListLayoutManager);
+        mUserListAdapter = new UserListAdapter(userList);
+        mUserList.setAdapter(mUserListAdapter);
+    }
+
+}
---
+++ app/src/main/java/com/lovettlowe/whatsappclone/UserObject.java
@@ -0,0 +1,16 @@
+package com.lovettlowe.whatsappclone;
+
+public class UserObject {
+
+    private String  name,
+                    phone;
+
+    public UserObject(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getPhone() { return phone; }
+    public String getName() { return name; }
+
+}
